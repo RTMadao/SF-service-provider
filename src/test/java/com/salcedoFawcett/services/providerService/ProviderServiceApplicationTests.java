3817f397ec8d94a2e00dffa1e7439c2c0f95b94b
Carlos Bello <bellocarlos2810@gmail.com>
2022-05-09 17:38:04 -0500

    set project structure

---
+++ src/test/java/com/salcedoFawcett/services/providerService/ProviderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salcedoFawcett.services.providerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
